4723d3482d68f683256d7929138ff2a4394bfbbb
Elias Almqvist <elalmqvist@gmail.com>
2023-06-20 21:09:54 +0200

    Loadbalancer

---
+++ src/loadbalancer.py
@@ -0,0 +1,74 @@
+import multiprocessing as mp
+
+
+# Worker function that processes tasks
+def worker(input_queue, output_pipe):
+    while True:
+        task = input_queue.get()  # Get a task from the input queue
+        if (
+            task is None
+        ):  # If task is None, it's a signal to exit the loop and terminate the worker process
+            break
+        # Process the task
+        result = process_task(
+            task
+        )  # Call the process_task function to perform the computation on the task
+        output_pipe.send(result)  # Send the result through the output pipe
+
+
+# Placeholder function for task processing logic
+def process_task(task):
+    return task
+
+
+# Placeholder function for handling the results
+def process_result(result):
+    # Handle the processed result
+    print("Result:", result)
+    # Return the result to the sender
+
+
+if __name__ == "__main__":
+    num_workers = 4  # Number of worker processes,  TODO: make config for this
+    input_queue = mp.Queue()  # Create a queue to store tasks
+
+    # Create multiple pipes for communication with the worker processes
+    output_pipes = [mp.Pipe() for _ in range(num_workers)]
+
+    workers = []  # List to store worker processes
+
+    # Start the worker processes
+    for i in range(num_workers):
+        # Create a worker process, passing the input queue and output pipe as arguments
+        worker_process = mp.Process(
+            target=worker, args=(input_queue, output_pipes[i][1])
+        )
+        worker_process.start()  # Start the worker process
+        workers.append(worker_process)  # Add the worker process to the list
+
+    tasks = []  # List of tasks to be processed
+
+    # Distribute tasks to the workers
+    for task in tasks:
+        input_queue.put(task)  # Put each task into the input queue
+
+    # Signal the worker processes to terminate
+    for _ in range(num_workers):
+        input_queue.put(
+            None
+        )  # Add a None task to the input queue for each worker process
+
+    results = []  # List to store the results
+
+    # Collect results from the worker processes
+    for i in range(num_workers):
+        result = output_pipes[i][0].recv()  # Receive the result from the output pipe
+        results.append(result)  # Add the result to the results list
+
+    # Wait for all worker processes to finish
+    for worker_process in workers:
+        worker_process.join()  # Wait for each worker process to terminate
+
+    # Process the results
+    for result in results:
+        process_result(result)  # Call the process_result function to handle each result
