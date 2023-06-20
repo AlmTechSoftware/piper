f0351ba282af06d771a46a970958825417f1fa9f
Elias Almqvist <elalmqvist@gmail.com>
2023-06-20 21:49:23 +0200

    More refactor

--- src/loadbalancer.py
+++ src/loadbalancer.py
@@ -1,78 +1,71 @@
 import multiprocessing as mp
-
-
-# Worker function that processes tasks
-def worker(input_queue, output_pipe):
-    while True:
-        task = input_queue.get()  # Get a task from the input queue
-        if (
-            task is None
-        ):  # If task is None, it's a signal to exit the loop and terminate the worker process
-            break
-        # Process the task
-        result = process_task(
-            task
-        )  # Call the process_task function to perform the computation on the task
-        output_pipe.send(result)  # Send the result through the output pipe
-
-
-# Placeholder function for task processing logic
-def process_task(task):
-    return task
-
-
-# Placeholder function for handling the results
-def process_result(result):
-    # Handle the processed result
-    print("Result:", result)
-    # Return the result to the sender
+from multiprocessing.connection import Connection
+from typing import Any, List
 
 
 class LoadBalancer:
-    def __init__(self, num_workers: int = 4):
-        self.input_queue = mp.Queue()  # Create a queue to store tasks
+    def __init__(self, input_pipe: mp.Queue, num_workers: int = 8) -> None:
+        self.input_pipe = input_pipe
+        self.num_workers = num_workers
+        self.output_pipes = []
+
+        self.workers = []
+        self.tasks = []
 
         # Create multiple pipes for communication with the worker processes
-        self.output_pipes = [mp.Pipe() for _ in range(num_workers)]
+        for _ in range(self.num_workers):
+            self.output_pipes.append(mp.Pipe())
 
-        self.__workers = []  # List to store worker processes
-        # Start the worker processes
-        for i in range(num_workers):
-            # Create a worker process, passing the input queue and output pipe as arguments
-            worker_process = mp.Process(
-                target=worker, args=(self.input_queue, self.output_pipes[i][1])
+    def start_workers(self) -> None:
+        for i in range(self.num_workers):
+            # Create a worker process, passing the input pipe and output pipe as arguments
+            worker_proc = mp.Process(
+                target=self.worker, args=(self.input_pipe, self.output_pipes[i][1])
             )
-            worker_process.start()  # Start the worker process
-            self.__workers.append(worker_process)  # Add the worker process to the list
+            worker_proc.start()  # Start the worker process
+            self.workers.append(worker_proc)  # Add the worker process to the list
 
-    def flush(self):
-        self.tasks = []  # List of tasks to be processed
+    def worker(self, input_pipe: mp.Queue, output_pipe: Connection) -> None:
+        while True:
+            task = input_pipe.recv()  # Receive a task from the input pipe
+            if (
+                task is None
+            ):  # If task is None, it's a signal to exit the loop and terminate the worker process
+                break
+            # Process the task
+            result = self.process_task(
+                task
+            )  # Call the process_task function to perform the computation on the task
+            output_pipe.send(result)  # Send the result through the output pipe
 
-        # Distribute tasks to the workers
-        for task in self.tasks:
-            self.input_queue.put(task)  # Put each task into the input queue
+    def process_task(self, task: Any) -> Any:
+        # Process the task and return the result
+        return task  # TODO: push into Piper algo
 
-        # Signal the worker processes to terminate
-        for _ in range(len(self.__workers)):
-            self.input_queue.put(
-                None
-            )  # Add a None task to the input queue for each worker process
-
+    def collect_results(self) -> List[Any]:
         results = []  # List to store the results
 
         # Collect results from the worker processes
-        for i in range(len(self.__workers)):
+        for i in range(self.num_workers):
             result = self.output_pipes[i][
                 0
             ].recv()  # Receive the result from the output pipe
             results.append(result)  # Add the result to the results list
 
+        return results
+
+    def wait_for_workers(self) -> None:
         # Wait for all worker processes to finish
-        for worker_process in self.__workers:
+        for worker_process in self.workers:
             worker_process.join()  # Wait for each worker process to terminate
 
+    def process_results(self, results: List[Any]) -> None:
         # Process the results
         for result in results:
-            process_result(
+            self.__process_result(
                 result
             )  # Call the process_result function to handle each result
+
+    def __process_result(self, result: Any) -> None:
+        # Placeholder function for handling the results
+        print("Result:", result)
--- src/server.py
+++ src/server.py
@@ -1,39 +1,45 @@
 import asyncio
-import websockets as ws
+from websockets.exceptions import ConnectionClosed
+from websockets.server import WebSocketServerProtocol, serve
+from typing import Any
+from loadbalancer import LoadBalancer
 
 
-async def handle_connection(websocket, path, input_queue):
-    try:
-        while True:
-            # Receive image frames from the WebSocket connection
-            data = await websocket.recv()
+class Server:
+    def __init__(self, load_balancer: LoadBalancer) -> None:
+        self.load_balancer = load_balancer
 
-            # Process the received data (e.g., convert it to a task)
-            task = process_received_data(data)
+    async def handle_connection(
+        self, websocket: WebSocketServerProtocol, _path: str
+    ) -> None:
+        try:
+            while True:
+                # Receive image frames from the WebSocket connection
+                data = await websocket.recv()
 
-            # Put the task into the input queue for processing
-            input_queue.put(task)
-    except ws.exceptions.ConnectionClosed:
-        pass
+                # Process the received data (e.g., convert it to a task)
+                task = self.process_received_data(data)
 
+                # Put the task into the input pipe of the load balancer for processing
+                self.load_balancer.input_pipe.send(task)
+        except ConnectionClosed:
+            # TODO: Discard all of the worker shit here
+            pass
 
-def process_received_data(data):
-    # Convert the data to the input type of the starting pipeline node
-    return data
+    def process_received_data(self, data: str) -> Any:
+        # Process input data here to np.array and feed into workers etc
+        return data
 
+    def start_websocket_server(self) -> None:
+        # WebSocket server configuration
+        host = "0.0.0.0"
+        port = 8000
 
-def start_websocket_server(input_queue, num_workers, output_pipes):
-    # WebSocket server configuration
-    host = "0.0.0.0"  # Set the host IP address
-    port = 8000  # Set the desired port number
+        # Start the WebSocket server
+        start_server = ws.serve(
+            lambda websocket, path: self.handle_connection(websocket, path), host, port
+        )
 
-    # Start the WebSocket server
-    start_server = ws.serve(
-        lambda websocket, path: handle_connection(websocket, path, input_queue),
-        host,
-        port,
-    )
-
-    # Event loop for the WebSocket server
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+        # Event loop for the WebSocket server
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
