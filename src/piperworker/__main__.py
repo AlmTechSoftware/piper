bff31d92454412e130aada28ce6e02864a8ba78c
Elias Almqvist <elalmqvist@gmail.com>
2023-06-21 16:16:41 +0200

    Added H264 codec decoder module

--- src/piperworker/__main__.py
+++ src/piperworker/__main__.py
@@ -1,7 +1,14 @@
+from os import environ
 import logging as log
 
 from cv_entrypoint import open_cv_frame_piper
 
 if __name__ == "__main__":
-    log.info("PiperWorker starting...")
-    open_cv_frame_piper()
+    worker_name = environ.get("PIPERWORKER_NAME")
+    log.info(f"Starting PiperWorker: {worker_name}")
+
+    # Build the full path to the named pipe
+    pipe_path = "/tmp/{worker_name}"
+
+    # Start the worker process
+    open_cv_frame_piper(pipe_path)
