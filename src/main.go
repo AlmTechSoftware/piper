089f47e60ec77a355812af19cf5aea39a875b25a
Elias Almqvist <elalmqvist@gmail.com>
2023-06-26 21:42:45 +0200

    Notes

--- src/main.go
+++ src/main.go
@@ -13,7 +13,8 @@ import (
 
 type channel = chan []byte
 
-func startWorkerProcs(numWorkers int, frameChan channel) ([]*os.Process, []string, error) {
+// TODO: REPLACE WITH CREATING OF NEW WORKERS WHEN NEEDED BECAUSE EZ
+func initWorkers(numWorkers int, frameChan channel) ([]*os.Process, []string, error) {
 	workers := make([]*os.Process, numWorkers)
 	socketPaths := make([]string, numWorkers)
 
@@ -55,10 +56,10 @@ func main() {
 	}
 
 	// WORKER COUNT
-	var numWorkersStr = os.Getenv("WORKER_COUNT")
+	var numWorkersStr = os.Getenv("MAX_WORKER_COUNT")
 	var numWorkers, err = strconv.Atoi(numWorkersStr)
 	if err != nil {
-		log.Fatalln("Unable to read worker count from environment! Please set \"WORKER_COUNT\" to a valid positive integer.")
+		log.Fatalln("Unable to read max worker count from the environment!\nPlease set \"MAX_WORKER_COUNT\" to a valid positive integer.")
 	}
 
 	// Start a TCP server to accept client connections
@@ -74,7 +75,7 @@ func main() {
 	// Start the worker processes
 	frameChan := make(chan []byte)
 
-	workers, socketPaths, err := startWorkerProcs(numWorkers, frameChan)
+	workers, socketPaths, err := initWorkers(numWorkers, frameChan)
 	if err != nil {
 		return
 	}
@@ -96,6 +97,7 @@ func main() {
 
 	var wg sync.WaitGroup
 	for {
+		// FIXME: handle clients and create new workers
 		// accept a new client connection
 		conn, err := lis.Accept()
 		if err != nil {
