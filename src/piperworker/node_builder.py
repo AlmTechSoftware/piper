0dd33cab0615bf2f3ce9af3122a5a5369a357d9f
jacksrevenge <jackblomquist@hotmail.com>
2023-07-01 16:55:00 +0200

    pasted the previous pipeline solution and corrected a minor bug

---
+++ src/piperworker/node_builder.py
@@ -0,0 +1,32 @@
+from ctypes import ArgumentError
+
+
+class Pipeline:
+    """
+    Basic "monad" like class for pipelining functions.
+    """
+
+    def __init__(self, nodes: list = []):
+        self.nodes = nodes
+
+    def link(self, other):
+        if type(other) == Pipeline:
+            return Pipeline(self.nodes + other.nodes)
+        else:
+            raise Exception("Cannot link a pipeline with a non-pipeline.")
+
+    def bind(self, node_function):
+        self.nodes.append(node_function)
+
+    def eval(self, *inputs):
+        input_proc = inputs
+
+        # Evaluate each node in pipeline order
+        for i, node_function in enumerate(self.nodes):
+            try:
+                print(input_proc)
+                input_proc = node_function(*input_proc)
+            except (ArgumentError, TypeError) as err:
+                raise Exception(f"Pipeline error at index {i-1}->{i}.\n{err}")
+
+        return input_proc
