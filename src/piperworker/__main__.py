d4d4bc8cf4c6daeaa56f948bb05188f948911aaf
Elias Almqvist <elalmqvist@gmail.com>
2023-07-02 02:21:30 +0200

    PiperWorker: multiproc

--- src/piperworker/__main__.py
+++ src/piperworker/__main__.py
@@ -4,17 +4,19 @@ import sys
 import numpy as np
 import logging as log
 import asyncio
-# from cv_entrypoint import open_cv_frame_piper
+
+socket_path = "/tmp/my_socket.socket"
+max_queue_size = 10
 
 
-def process_frame(frame_data_bytes):
-    # TODO: Process the received frame data
-
+async def process_frame(frame_data_bytes):
     # Convert the frame data to a NumPy array
     frame_array = np.frombuffer(frame_data_bytes, dtype=np.uint8)
 
     # Decode the H.264 frame using OpenCV
     new_frame = cv2.imdecode(frame_array, cv2.IMREAD_UNCHANGED)
+
+    # TODO: Perform asynchronous video processing operations
 
     # Send back the frame in byte format
     return new_frame.tobytes()
@@ -24,6 +26,12 @@ async def handle_client(reader, writer):
     client_address = writer.get_extra_info("peername")
     print(f"Accepted connection from {client_address}")
 
+    # Create an asyncio Queue to store the video frames
+    video_queue = asyncio.Queue(maxsize=max_queue_size)
+
+    # Start the video processing task
+    processing_task = asyncio.create_task(process_video_frames(video_queue, writer))
+
     try:
         while True:
             # Read data from the client
@@ -31,31 +39,49 @@ async def handle_client(reader, writer):
             if not data:
                 break
 
-            # Process data
-            new_frame = process_frame(data)
-
-            # Send back the processed data
-            writer.write(new_frame)
-            await writer.drain()
+            # Put the received frame into the video queue
+            try:
+                await video_queue.put(data)
+            except asyncio.QueueFull:
+                print(f"Video queue is full, dropping frame...")
 
     except Exception as e:
         print(f"Error occurred: {e}")
 
     finally:
+        # Stop the video processing task
+        processing_task.cancel()
+        try:
+            await processing_task
+        except asyncio.CancelledError:
+            pass
+
         # Close the client connection
         writer.close()
         print(f"Closed connection from {client_address}")
 
 
-async def start_worker(socket_path: str):
-    if os.path.exists(socket_path):
-        os.remove(socket_path)
+async def process_video_frames(video_queue, writer):
+    while True:
+        # Get the next frame from the video queue
+        frame_data = await video_queue.get()
 
+        # Process the frame asynchronously
+        processed_frame = await process_frame(frame_data)
+
+        # Send back the processed frame to the client (optional)
+        writer.write(processed_frame)
+        await writer.drain()
+
+        # Example: Print the size of the processed frame
+        print(f"Processed frame: {len(processed_frame)} bytes")
+
+
+async def start_worker(socket_path: str):
     server = await asyncio.start_unix_server(handle_client, path=socket_path)
 
     async with server:
         await server.serve_forever()
-    
 
 
 if __name__ == "__main__":
@@ -66,6 +92,10 @@ if __name__ == "__main__":
         exit(1)
 
     socket_path = sys.argv[1]
-    print(f"Starting PiperWorker on socket \"{socket_path}\"")
+
+    if os.path.exists(socket_path):
+        os.remove(socket_path)
+
+    print(f'Starting PiperWorker on socket "{socket_path}"')
 
     asyncio.run(start_worker(socket_path))
