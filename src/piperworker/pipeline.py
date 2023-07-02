1d4d9b0da3d6f7528d6ac30c86c5e54f295bbec3
Jack Blomquist <74712554+jacksrevenge@users.noreply.github.com>
2023-07-02 02:29:51 +0200

    Node builder (aka Pipeline) (#19)
    * added node_builder.py
    
    * added node_builder.py
    
    * pasted the previous pipeline solution and corrected a minor bug
    
    * cleaned up the code
    
    * err -> error
    
    * changed name of the file to from node_builder to pipeline
    
    ---------
    
    Co-authored-by: Elias Almqvist <elalmqvist@gmail.com>

---
+++ src/piperworker/pipeline.py
@@ -0,0 +1,31 @@
+from ctypes import ArgumentError
+
+
+class Pipeline:
+    """
+    Basic "monad" like class for pipelining the node functions.
+    """
+
+    def __init__(self, nodes: list = []):
+        self.nodes = nodes
+
+    def link(self, other):
+        if type(other) == Pipeline:
+            return Pipeline(self.nodes + other.nodes)
+        else:
+            raise Exception("Cannot link a pipeline with a non-pipeline.")
+
+    def bind(self, node_function):
+        self.nodes.append(node_function)
+
+    def eval(self, *inputs):
+        input_proc = inputs
+
+        # Evaluate each node in pipeline order
+        for i, node_function in enumerate(self.nodes):
+            try:
+                input_proc = node_function(*input_proc)
+            except (ArgumentError, TypeError) as error:
+                raise Exception(f"Pipeline error at index {i-1}->{i}.\n{error}")
+
+        return input_proc
