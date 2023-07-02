d0bc8e5d3aad80bc233048a04c42be2db5167164
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2023-07-02 14:00:18 +0200

    mv binder

---
+++ src/piperworker/binder.py
@@ -0,0 +1,21 @@
+from .pipeline import Pipeline
+
+# Seudo code, but the basis
+# import canvas
+# import CDNN
+# import CEMNN
+# import ColorRemap
+# import CRAD
+# import SGNN
+# import vect
+
+pipeline = Pipeline
+
+# Mockup for pipeline
+# pipeline.bind(canvas.entrypoint())
+# pipeline.bind(CDNN.entrypoint())
+# pipeline.bind(CEMNN.entrypoint())
+# pipeline.bind(ColorRemap.entrypoint())
+# pipeline.bind(CRAD.entrypoint())
+# pipeline.bind(SGNN.entrypoint())
+# pipeline.bind(vect.entrypoint())
