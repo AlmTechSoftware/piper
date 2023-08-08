18d32b5714b584efebf87e4717ff6a840ff285d8
Elias Almqvist <elalmqvist@gmail.com>
2023-08-08 22:12:12 +0200

    ref: Lesser network + sequential

--- training_data/feynman_torch.py
+++ training_data/feynman_torch.py
@@ -10,80 +10,58 @@ from dataset_handler import COCODataset
 
 
 class FeynmanModel(nn.Module):
-    def __init__(self):
-        super(FeynmanModel, self).__init__()
-        self.model = self.build_model()
-
-    def build_model(self):
+    def __init__(self, num_classes: int = 3):
+        super(self.__class__, self).__init__()
+        
         # Encoder
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv7 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        self.conv8 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        # Middle
+        self.middle = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
         # Decoder
-        self.up1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.conv9 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.conv10 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.conv11 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.conv12 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.up3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.conv13 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.conv14 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-
-        # Output
-        self.output = nn.Conv2d(64, 1, kernel_size=1)
-
+        self.decoder = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, num_classes, kernel_size=1)
+        )
+        
     def forward(self, x):
         # Encoder
-        x1 = nn.ReLU()(self.conv1(x))
-        x2 = nn.ReLU()(self.conv2(x1))
-        pool1 = nn.MaxPool2d(kernel_size=2, stride=2)(x2)
-
-        x3 = nn.ReLU()(self.conv3(pool1))
-        x4 = nn.ReLU()(self.conv4(x3))
-        pool2 = nn.MaxPool2d(kernel_size=2, stride=2)(x4)
-
-        x5 = nn.ReLU()(self.conv5(pool2))
-        x6 = nn.ReLU()(self.conv6(x5))
-        pool3 = nn.MaxPool2d(kernel_size=2, stride=2)(x6)
-
-        # Bottleneck
-        x7 = nn.ReLU()(self.conv7(pool3))
-        x8 = nn.ReLU()(self.conv8(x7))
-
+        x1 = self.encoder(x)
+        
+        # Middle
+        x2 = self.middle(x1)
+        
         # Decoder
-        up1 = self.up1(x8)
-        concat1 = torch.cat((up1, x6), dim=1)
-        x9 = nn.ReLU()(self.conv9(concat1))
-        x10 = nn.ReLU()(self.conv10(x9))
+        x3 = self.decoder(x2)
+        
+        # Upsample to original size
+        x3 = nn.functional.interpolate(x3, scale_factor=2, mode='bilinear', align_corners=False)
+        
+        return x3
 
-        up2 = self.up2(x10)
-        concat2 = torch.cat((up2, x4), dim=1)
-        x11 = nn.ReLU()(self.conv11(concat2))
-        x12 = nn.ReLU()(self.conv12(x11))
-
-        up3 = self.up3(x12)
-        concat3 = torch.cat((up3, x2), dim=1)
-        x13 = nn.ReLU()(self.conv13(concat3))
-        x14 = nn.ReLU()(self.conv14(x13))
-
-        # Output
-        output = nn.Sigmoid()(self.output(x14))
-
-        return output
-
-    def load_data(self, dataset_dir: str):
+    @staticmethod
+    def load_data(dataset_dir: str):
         # Load the COCO dataset
         dataset = COCODataset(dataset_dir=dataset_dir)
         return dataset
 
-    def train(self, dataset_dir, epochs=10, batch_size=32):
+    def train(self, dataset_dir: str, epochs=10, batch_size=32):
         criterion = nn.BCELoss()
         optimizer = optim.Adam(self.parameters(), lr=0.001)
 
@@ -104,5 +82,5 @@ class FeynmanModel(nn.Module):
 
 
 # Usage:
-model = FeynmanModel()
+model = FeynmanModel(3)
 model.train(dataset_dir="./dataset/train/", epochs=10, batch_size=32)
