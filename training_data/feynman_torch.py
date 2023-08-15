f5305bc0bf4a64fd876424913f51c70330af6b26
Elias Almqvist <elalmqvist@gmail.com>
2023-08-15 20:43:13 +0200

    ref: use resnet50 with custom dataset

--- training_data/feynman_torch.py
+++ training_data/feynman_torch.py
@@ -1,76 +1,16 @@
-import logging
 import torch.nn as nn
-import torch.optim as optim
 
-from torch import Tensor
-
-# import numpy as np
-
-from torch.utils.data import DataLoader
-
-from dataset_handler import COCODataset
+import torchvision.models.segmentation as models
 from segnet import *
 
 
-# Model definition. We use a SegNet-Basic model with some minor tweaks.
-# Our input images are 128x128.
 class FeynmanModel(nn.Module):
-    def __init__(self, kernel_size: int):
-        super().__init__()
-        self.out_channels = 3
-        self.bn_input = nn.BatchNorm2d(3)
+    def __init__(self, num_classes):
+        super(__class__, self).__init__()
+        self.backbone = models.fcn_resnet50(pretrained=True, progress=True)
+        self.backbone.classifier[4] = nn.Conv2d(
+            512, num_classes, kernel_size=(1, 1), stride=(1, 1)
+        )
 
-        # Encoder
-        self.dc1 = DownConv2(3, 64, kernel_size=kernel_size)
-        self.dc2 = DownConv2(64, 128, kernel_size=kernel_size)
-        self.dc3 = DownConv3(128, 256, kernel_size=kernel_size)
-        self.dc4 = DownConv3(256, 512, kernel_size=kernel_size)
-
-        # Decoder
-        self.uc4 = UpConv3(512, 256, kernel_size=kernel_size)
-        self.uc3 = UpConv3(256, 128, kernel_size=kernel_size)
-        self.uc2 = UpConv2(128, 64, kernel_size=kernel_size)
-        self.uc1 = UpConv2(64, 3, kernel_size=kernel_size)
-
-    def forward(self, batch: Tensor):
-        x = self.bn_input(batch)
-        x, mp1_indices, shape1 = self.dc1(x)
-        x, mp2_indices, shape2 = self.dc2(x)
-        x, mp3_indices, shape3 = self.dc3(x)
-        x, mp4_indices, shape4 = self.dc4(x)
-        # x, mp5_indices, shape5 = self.dc5(x)
-
-        # SegNet Decoder
-        # x = self.uc5(x, mp5_indices, output_size=shape5)
-        x = self.uc4(x, mp4_indices, output_size=shape4)
-        x = self.uc3(x, mp3_indices, output_size=shape3)
-        x = self.uc2(x, mp2_indices, output_size=shape2)
-        x = self.uc1(x, mp1_indices, output_size=shape1)
-
-        return x
-
-    @staticmethod
-    def load_data(dataset_dir: str):
-        # Load the COCO dataset
-        dataset = COCODataset(dataset_dir=dataset_dir)
-        return dataset
-
-    def train(self, dataset_dir: str, epochs=10, batch_size=32):
-        criterion = nn.BCELoss()
-        optimizer = optim.Adam(self.parameters(), lr=0.001)
-
-        dataset = self.load_data(dataset_dir)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-        for epoch in range(epochs):
-            for i, (batch_images, batch_masks) in enumerate(dataloader):
-                logging.debug(f"{epoch+1} - {i+1}")
-                optimizer.zero_grad()
-                outputs = self.forward(batch_images)
-                loss = criterion(outputs, batch_masks)
-                loss.backward()
-                optimizer.step()
-
-                logging.debug(
-                    f"\tEpoch [{epoch + 1}/{epochs}], Batch [{i + 1}/{len(dataloader)}], Loss: {loss.item()}"
-                )
+    def forward(self, x):
+        return self.backbone(x)["out"]
--- training_data/train.py
+++ training_data/train.py
@@ -1,21 +1,75 @@
 #!/usr/bin/env python
 
 import logging
+import torch
+from torch.utils.data import DataLoader
 from feynman_torch import FeynmanModel
 
 from colored import Fore, Back, Style
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from dataset_handler import COCODataset
+
 
 logging.basicConfig(
     level=logging.DEBUG,
     format=f"{Back.rgb(80, 200, 80)} FeynMAN {Style.reset} [{Fore.rgb(255, 240, 240)}%(levelname)s{Style.reset}] %(message)s",
 )
 
+def load_data(dataset_dir: str):
+    # Load the COCO dataset
+    dataset = COCODataset(dataset_dir=dataset_dir)
+    return dataset
+
+def train_model(model, dataset_dir: str, epochs=10, batch_size=32):
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    dataset = load_data(dataset_dir)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+
+        for batch_idx, (images, masks) in enumerate(dataloader):
+            images, masks = images.to(device), masks.to(device).long()
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, masks.squeeze(1))
+
+            # Backpropagation and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if batch_idx % 10 == 9:  # Print every 10 batches
+                print(
+                    f"Epoch [{epoch+1}/{epochs}] Batch [{batch_idx+1}/{len(dataloader)}] Loss: {running_loss/10:.4f}"
+                )
+                running_loss = 0.0
+
+
 def main():
     # Usage:
     logging.info("Creating model...")
-    model = FeynmanModel(3)
+    model = FeynmanModel(6)
+
+    # device_type = "cuda" if torch.cuda.is_available() else "cpu"
+    # logging.debug(f"Doing training on device type '{device_type}'!")
+    # device = torch.device(device_type)
+    #
+    # model.to(device)
+
     logging.info("START TRAINING!")
-    model.train(dataset_dir="./dataset/train/", epochs=10, batch_size=2)
+    train_model(model, dataset_dir="./dataset/train/", epochs=10, batch_size=16)
     logging.info("END TRAINING!")
 
 
