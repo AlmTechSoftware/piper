deffd3e49707502d58058d004beb72fefadc1107
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2023-09-25 20:22:17 +0200

    updated init

--- src/piper/piperworker/nodes/CRAD/__init__.py
+++ src/piper/piperworker/nodes/CRAD/__init__.py
@@ -1,4 +1,4 @@
-from .node import remove_perspective, canvas_contour, canvas_render
+from .node import remove_perspective, canvas_contour
 
 
 def entrypoint(frame, points):
@@ -8,6 +8,5 @@ def entrypoint(frame, points):
 
     crad_frame = remove_perspective(frame, points)
     crad_frame, frame_contours = canvas_contour(crad_frame)
-    crad_frame = canvas_render(crad_frame, frame_contours)
 
-    return crad_frame
+    return crad_frame, frame_contours
